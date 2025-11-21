f47864f81c12fb1afbc5fb02a3b421a86d9295bb
Enes Cakici <enescakici65@gmail.com>
2025-11-21 15:18:11 +0300

    Add project files.

---
+++ SporSalonuYonetim/Models/Service.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SporSalonuYonetim.Models
+{
+    public class Service
+    {
+        [Key]
+        public int ServiceId { get; set; }
+
+        [Required]
+        [Display(Name ="Hizmet Adı")]
+        public string ServiceName { get; set; }
+
+        [Display(Name = "Süre")]
+        public int DurationMinutes { get; set; }
+
+        [Display(Name = "Ücret" )]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
---
+++ SporSalonuYonetim/Models/Trainer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SporSalonuYonetim.Models
+{
+    public class Trainer
+    {
+        [Key]  // primery key
+        public int TrainerId { get; set; }
+
+        [Required(ErrorMessage = "Ad Soyad Girilmesi Zorunludur.")]
+        [Display(Name = "Adı Soyadı")]
+        public string TrainerName { get; set; }
+
+        [Required(ErrorMessage = "Uzmanlık Alanı Girilmesi Zorunludur.")]
+        [Display(Name = "Uzmanlık Alanı")]
+        public string Specialization { get; set; }  //uzmanlık
+
+        [Display(Name = "Fotograf")]
+        public string? ImageUrl { get; set; }  //fotografyolu
+
+    }
+}
